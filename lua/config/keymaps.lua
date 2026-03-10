e5c2118e08e1f9d619034b225c2ac6671dac71a1
Caleb Land <caleb@land.fm>
2026-03-10 14:55:45 -0400

    Add more convenient keymaps for terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Terminal: Escape exits terminal mode, C-Esc sends a literal Escape to the terminal
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+vim.keymap.set("t", "<Esc><Esc>", "<Esc>", { desc = "Send Escape to terminal" })
